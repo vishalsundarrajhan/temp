7a94328fbc67b445b8d5ce04b0e2051566326adc
vishalsundarrajhan <vishalsrpersonal27@gmail.com>
2026-04-07 13:31:46 +0530

    Pushing client/src/main.tsx via API

---
+++ client/src/main.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
+import { LanguageProvider } from './contexts/LanguageContext.tsx'
+import { ThemeProvider } from './contexts/ThemeContext.tsx'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <ThemeProvider>
+      <LanguageProvider>
+        <App />
+      </LanguageProvider>
+    </ThemeProvider>
+  </React.StrictMode>,
+)
